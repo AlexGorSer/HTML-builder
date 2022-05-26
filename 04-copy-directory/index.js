35f31b2b9135d5f328cce2ad0f9d4cf011b8c7b9
AlexGorSer <taskekaminari@gmail.com>
2022-05-26 17:48:10 +0200

    feat: fix 04 and 05

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,6 +8,7 @@ fs.rm(copyFolder, {force:true, recursive: true}, (err) =>{
   fs.mkdir(copyFolder, {recursive: true}, (err) =>{
     if (err) throw err;
     fs.readdir(srcFolder, {withFileTypes:true}, (err, files) =>{
+      if (err) throw err;
       files.forEach(file => {
         fs.copyFile(path.resolve(srcFolder, file.name), path.resolve(copyFolder, file.name), (err) => {
           if (err) throw err;
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,16 +5,21 @@ const style = path.resolve(__dirname, 'styles');
 const projectDist = path.resolve(__dirname, 'project-dist');
 const bundle = fs.createWriteStream(path.resolve(projectDist, 'bundle.css'));
 
-fs.readdir(style, (err, files) => {
+fs.readdir(style, { withFileTypes: true }, (err, files) => {
   if (err) throw err;
 
   files.forEach((file) => {
-    if(path.extname(file) === '.css') {
-      const read = fs.createReadStream(path.resolve(style, file));
-      read.on('data', (data) => {
-        bundle.write(data + '\n');
-      });
+    if(file.isFile()){
+      const fileName = path.resolve(style, file.name);
+      if(path.extname(fileName) === '.css'){
+        const read = fs.createReadStream(path.resolve(style, fileName));
+        read.on('data', (data) => {
+          bundle.write(data + '\n');
+        });
+      }
+
     }
   });
 });
+
 
